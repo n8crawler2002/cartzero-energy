214e25bd7abe7dd6b6dd6eadd0bff4e69f0d7aaa
n8crawler2002 <n8crawler2002@googlemail.com>
2026-05-17 18:01:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,50 @@
-const CACHE = 'cartzero-v1';
-const ASSETS = [
+const CACHE = 'cz-v3';
+const SHELL = [
   '/',
-  '/join',
-  '/dashboard',
-  '/map',
-  '/mycart',
-  '/discover',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/index.html',
+  '/join.html',
+  '/dashboard.html',
+  '/map.html',
+  '/mycart.html',
+  '/discover.html',
+  '/tracker.html'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  const url = new URL(e.request.url);
+
+  // tracker.json: always network-first (source of truth)
+  if(url.pathname.includes('tracker.json')){
+    e.respondWith(
+      fetch(e.request)
+        .then(res => {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+          return res;
+        })
+        .catch(() => caches.match(e.request))
+    );
+    return;
+  }
+
+  // Shell files: cache-first
+  if(SHELL.some(s => url.pathname === s || url.pathname.startsWith(s))){
+    e.respondWith(
+      caches.match(e.request).then(hit => hit || fetch(e.request))
+    );
+  }
 });
